2fa75f65a455247d3de2c46b657d3ea5d3ccde45
Claudia Iturregui <claudia.iturregui@gmail.com>
2023-02-13 12:17:39 -0500

    second commit

--- basic-auth/routes/index.js
+++ basic-auth/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Claudias Animal Catalogue' });
+  res.render('index', { title: 'Adopt a Best Friend' });
 });
 
 

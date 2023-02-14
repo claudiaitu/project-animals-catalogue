c21018f5e0b5d5062d5e7f65688c8cd2558a1e07
Claudia Iturregui <claudia.iturregui@gmail.com>
2023-02-14 13:36:16 -0500

    middleware and clickable logo

--- basic-auth/middleware/route-guard.js
+++ basic-auth/middleware/route-guard.js
@@ -1,3 +1,6 @@
+const Pet = require("../models/pet.model");
+
+
 const isLoggedIn = (req, res, next) => {
     if (!req.session.user) {
        return res.redirect('/login/userLogin')
@@ -13,7 +16,27 @@ const isLoggedOut = (req, res, next) => {
     next();
 }
 
+
+const isOwner = (req, res, next) => {
+
+    Pet.findById(req.params.petId)
+    .populate('owner')
+    .then((foundPet) => {
+        console.log(foundPet);
+        if (!req.session.user || foundPet.owner._id.toString() !== req.session.user._id) {
+            res.redirect('/allPets')
+        } else {
+            next()
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+}
+
 module.exports = {
     isLoggedIn,
-    isLoggedOut
+    isLoggedOut,
+    isOwner
 }
--- basic-auth/routes/all.pets.routes.js
+++ basic-auth/routes/all.pets.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Pet = require("../models/pet.model");
+const { isOwner } = require("../middleware/route-guard")
 
 router.get('/', (req, res, next) => {
     Pet.find()
@@ -11,17 +12,18 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.get('/allPet/:petId/edit', (req, res, next) => {
+router.get('/allPet/:petId/edit', isOwner, (req, res, next) => {
     const { petID } = req.params;
    
     Pet.findById(petID)
       .then(petToEdit => {
         res.render('login/pet-edit.hbs', { Pet: petToEdit }); // <-- add this line
+
     })
       .catch(error => next(error));
   });
 
-  router.post('/allPet/:petId/edit', (req, res, next) => {
+  router.post('/allPet/:petId/edit', isOwner, (req, res, next) => {
     const { petId } = req.params;
     const { name, species, feeding, environment } = req.body;
    
@@ -31,7 +33,7 @@ router.get('/allPet/:petId/edit', (req, res, next) => {
   });
 
 
-  router.post('/allPet/:petId/delete', (req, res, next) => {
+  router.post('/allPet/:petId/delete', isOwner, (req, res, next) => {
     const { petId } = req.params;
    
     Pet.findByIdAndDelete(petId)
